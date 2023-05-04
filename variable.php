b04d33402985bfc5e48507cc787d3e52932024e2
kamrulerp <kamrulerp@gmail.com>
2023-05-04 20:58:23 +0600

    first commit

---
+++ variable.php
@@ -0,0 +1,18 @@
+<?php
+
+ $var = 'Bob';
+ $varInt = 12;
+ $varFloat = 12.5;
+
+ 
+ echo $var;
+
+ echo 'summation of 2 + 2 = '.(2 + 2).'<br>';
+ echo " summation of 2 + 2 = '$varFloat' <br>";
+ echo " summation of 2 + 2 = ".$varFloat." <br>";
+ echo ' summation of 2 + 2 = "$varFloat" <br>';
+
+ 
+ ?>
+
+ <?= $var ?>
